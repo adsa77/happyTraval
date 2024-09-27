28096fe996af5ab9f4a768d3c5ed1965c53f2629
DESKTOP-I24B9KE\adsa7 <adsa77@naver.com>
2024-09-28 05:40:31 +0900

    기본설정

---
+++ src/main/java/com/happyTraval/HappyTravalApplication.java
@@ -0,0 +1,13 @@
+package com.happyTraval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyTravalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyTravalApplication.class, args);
+	}
+
+}
